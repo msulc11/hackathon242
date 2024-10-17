42703696245898f735396c615b2b8764dc7d746c
msulc11 <matej.sulcu@gmail.com>
2024-10-17 16:47:18 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "iNVESTRACK KHK",
+  description: "iNVESTRACK KHK",
 };
 
 export default function RootLayout({
