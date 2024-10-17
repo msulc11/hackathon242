2abc81816a77acd736535449d44e750b771ef9af
msulc11 <matej.sulcu@gmail.com>
2024-10-17 10:27:25 +0200

    changes1

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -8,32 +8,32 @@ import Link from "next/link";
 export default function Login({ searchParams }: { searchParams: Message }) {
   return (
     <form className="flex-1 flex flex-col min-w-64">
-      <h1 className="text-2xl font-medium">Sign in</h1>
+      <h1 className="text-2xl font-medium">Přihlášení</h1>
       <p className="text-sm text-foreground">
-        Don't have an account?{" "}
+        Nemáte účet?{" "}
         <Link className="text-foreground font-medium underline" href="/sign-up">
-          Sign up
+          Registrace
         </Link>
       </p>
       <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
         <Label htmlFor="email">Email</Label>
-        <Input name="email" placeholder="you@example.com" required />
+        <Input name="email" placeholder="Váš email" required />
         <div className="flex justify-between items-center">
           <Label htmlFor="password">Password</Label>
           <Link
             className="text-xs text-foreground underline"
             href="/forgot-password"
           >
-            Forgot Password?
+            Zapomenute heslo?
           </Link>
         </div>
         <Input
           type="password"
           name="password"
-          placeholder="Your password"
+          placeholder="Vaše heslo"
           required
         />
-        <SubmitButton pendingText="Signing In..." formAction={signInAction}>
+        <SubmitButton pendingText="Přihlašuji se..." formAction={signInAction}>
           Sign in
         </SubmitButton>
         <FormMessage message={searchParams} />
--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -4,7 +4,7 @@ import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
-import { SmtpMessage } from "../smtp-message";
+
 
 export default function Signup({ searchParams }: { searchParams: Message }) {
   if ("message" in searchParams) {
@@ -18,31 +18,31 @@ export default function Signup({ searchParams }: { searchParams: Message }) {
   return (
     <>
       <form className="flex flex-col min-w-64 max-w-64 mx-auto">
-        <h1 className="text-2xl font-medium">Sign up</h1>
+        <h1 className="text-2xl font-medium">Registrace</h1>
         <p className="text-sm text text-foreground">
-          Already have an account?{" "}
+          Už máte účet?{" "}
           <Link className="text-primary font-medium underline" href="/sign-in">
-            Sign in
+            Přihlášení
           </Link>
         </p>
         <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
           <Label htmlFor="email">Email</Label>
-          <Input name="email" placeholder="you@example.com" required />
-          <Label htmlFor="password">Password</Label>
+          <Input name="email" placeholder="Váš email" required />
+          <Label htmlFor="password">Heslo</Label>
           <Input
             type="password"
             name="password"
-            placeholder="Your password"
+            placeholder="Vaše heslo"
             minLength={6}
             required
           />
-          <SubmitButton formAction={signUpAction} pendingText="Signing up...">
-            Sign up
+          <SubmitButton formAction={signUpAction} pendingText="Registruji se...">
+            Registrovat se
           </SubmitButton>
           <FormMessage message={searchParams} />
         </div>
       </form>
-      <SmtpMessage />
+
     </>
   );
 }
