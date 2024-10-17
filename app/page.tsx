4a1b7607db0be6e396499052a2960a25f977fe71
msulc11 <matej.sulcu@gmail.com>
2024-10-17 11:41:34 +0200

    upravyahaaha

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,7 @@
-// app/page.tsx
-import fs from 'fs';
-import path from 'path';
+import React from 'react'
 
-interface GeoJSONFeature {
-  type: string;
-  geometry: {
-    type: string;
-    coordinates: number[];
-  };
-  properties: {
-    title: string;
-    description: string;
-  };
-}
-
-interface GeoJSON {
-  type: string;
-  features: GeoJSONFeature[];
-}
-
-const fetchGeoJSON = async (): Promise<GeoJSON> => {
-  // Define the path to the GeoJSON file
-  const filePath = path.join(process.cwd(), 'json', 'investice.geojson');
-
-  // Read the GeoJSON file
-  const geojsonData = fs.readFileSync(filePath, 'utf-8');
-
-  // Parse the GeoJSON data
-  return JSON.parse(geojsonData);
-};
-
-export default async function Home() {
-  const geojson = await fetchGeoJSON();
-  const features = geojson.features;
-
+export default function page() {
   return (
-    <div>
-      <h1>Companies List</h1>
-      <ul>
-        {geojson.features.map((feature) => (
-          <li key={feature.id}>
-            <strong>{feature.properties.nazev_spolecnosti}</strong><br />
-            Ulice: {feature.properties.nazev_ulice} {feature.properties.cislo_domovni}<br />
-            Město: {feature.properties.nazev_obce}, PSČ: {feature.properties.psc}<br />
-            Země původu: {feature.properties.země_puvodu_zadatele}<br />
-            IČO: {feature.properties.ico}<br />
-            <a href={feature.properties.www} target="_blank" rel="noopener noreferrer">
-              Web
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    <div>Ahoj</div>
+  )
 }
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -2,10 +2,43 @@ import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
 import { createClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
+import fs from 'fs';
+import path from 'path';
 
+interface GeoJSONFeature {
+  type: string;
+  geometry: {
+    type: string;
+    coordinates: number[];
+  };
+  properties: {
+    title: string;
+    description: string;
+  };
+}
+
+interface GeoJSON {
+  type: string;
+  features: GeoJSONFeature[];
+}
+
+const fetchGeoJSON = async (): Promise<GeoJSON> => {
+  // Define the path to the GeoJSON file
+  const filePath = path.join(process.cwd(), 'json', 'investice.geojson');
+
+  // Read the GeoJSON file
+  const geojsonData = fs.readFileSync(filePath, 'utf-8');
+
+  // Parse the GeoJSON data
+  return JSON.parse(geojsonData);
+};
+
+ 
 export default async function ProtectedPage() {
   const supabase = createClient();
 
+  const geojson = await fetchGeoJSON();
+  const features = geojson.features;
   const {
     data: { user },
   } = await supabase.auth.getUser();
@@ -15,8 +48,22 @@ export default async function ProtectedPage() {
   }
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-12">
-   
+    <div>
+      <h1>Companies List</h1>
+      <ul>
+        {geojson.features.map((feature) => (
+          <li key={feature.id}>
+            <strong>{feature.properties.nazev_spolecnosti}</strong><br />
+            Ulice: {feature.properties.nazev_ulice} {feature.properties.cislo_domovni}<br />
+            Město: {feature.properties.nazev_obce}, PSČ: {feature.properties.psc}<br />
+            Země původu: {feature.properties.země_puvodu_zadatele}<br />
+            IČO: {feature.properties.ico}<br />
+            <a href={feature.properties.www} target="_blank" rel="noopener noreferrer">
+              Web
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
