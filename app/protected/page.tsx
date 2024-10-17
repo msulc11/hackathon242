8fc73d9dc9e945daa778d98849f4140eccadacd1
Daniel Kvapil <danek.kvapil@gmail.com>
2024-10-17 16:34:55 +0200

    upravy

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -12,13 +12,13 @@ export default function Page() {
         style={{ backgroundImage: 'url("/images/financial.webp")' }}
       >
         <div className="text-center p-5 bg-[#0b0a0b] bg-opacity-60 dark:bg-[#0b0a0b] dark:bg-opacity-80 rounded-lg">
-          <h1 className="text-5xl font-bold mb-4">Welcome to Financial Companies Hub</h1>
-          <p className="text-xl mb-6">Your source for financial insights, company data, and investment tracking</p>
+          <h1 className="text-5xl font-bold mb-4">Vítejte na iNVESTRACK KHK</h1>
+          <p className="text-xl mb-6">Objevte společnosti, které využili investiční nabídky!</p>
           <Link
             href="/protected/Map"
             className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded-full"
           >
-            Explore Companies
+            Objevte společnosti
           </Link>
         </div>
       </header>
@@ -26,18 +26,34 @@ export default function Page() {
       {/* Information Section */}
       <section className="info-section py-16 bg-white dark:bg-[#0b0a0b]">
         <div className="max-w-5xl mx-auto px-5">
-          <h2 className="text-3xl font-semibold text-center mb-8 dark:text-white">About Our Platform</h2>
+          <h2 className="text-3xl font-semibold text-center mb-8 dark:text-white">Co děláme?</h2>
           <p className="text-gray-700 dark:text-gray-300 text-lg leading-relaxed mb-8">
-            Welcome to Financial Companies Hub, where you can access comprehensive information about companies and their financial health. Whether you're an investor, analyst, or just curious about financial markets, we provide tools and data to help you make informed decisions.
+          Objevte potenciál investičních pobídek s naší aplikací! iNVESTACK KHK využívá otevřená data od CzechInvest, aby vám poskytla všechny potřebné informace o daňových úlevách a dotacích, které mohou podpořit vaše podnikání. Nechte stát investovat do vašeho úspěchu a rozvíjejte svůj byznys v technologických centrech, výrobních závodech či strategických službách.
           </p>
           <p className="text-gray-700 dark:text-gray-300 text-lg leading-relaxed mb-8">
-            Our platform tracks key financial metrics, company performance, and industry trends to keep you updated. Explore our database of companies, review financial data, and track investments, all in one place.
-          </p>
-          <p className="text-gray-700 dark:text-gray-300 text-lg leading-relaxed">
-            Join us today and unlock a wealth of financial insights that will help you stay ahead in the competitive world of finance.
+          iNVESTRACK KHK vám ukáže, jak investiční pobídky mohou změnit váš podnikatelský život. Podpora podnikatelů a firem nejenže přináší finanční růst, ale také kultivuje a inovuje celý region. Naše aplikace nabízí přehled podmínek jejich získání, schvalovaných Ministerstvem průmyslu a obchodu. Připojte se k těm, kteří už využili investiční pobídky k rozvoji svého podnikání a pracovních míst.
           </p>
         </div>
       </section>
+
+      <section className="info-section py-16 bg-white dark:bg-[#0b0a0b]">
+        <div className="max-w-5xl mx-auto px-5">
+          <h2 className="text-3xl font-semibold text-center mb-8 dark:text-white">Kroky k získání investiční pobídky</h2>
+          <p className="text-gray-700 dark:text-gray-300 text-lg leading-relaxed mb-8">
+          - Zjistit, zda se nacházím v určité oblasti investorů <br />
+          - Kontaktovat sprostředkovatele (CzechInvest) <br />
+          - Vyplnit žádost <br />
+          - Doložit potřebné doklady <br />
+          Sprostředkovatel předá žádost ministerstvu průmyslu a obchodu. Žádost putuje k vládě ke schválení.
+          </p>
+          <h2 className="text-2xl font-semibold mb-8 dark:text-white">Podle čeho se investiční pobídky posuzují?</h2>
+          <p className="text-gray-700 dark:text-gray-300 text-lg leading-relaxed mb-8">
+            Stát zohledňuje přínos projektu pro region → v žádosti musí být předpokládaný odvod do veřejných rozpočtů či přínos na trh práce. <br />
+            Dále musí být definováno jakým způsobem chtějí zakomponovat výzkum a vývoj nebo spolupráci s místními institucemi a školami. Ve zkratce musí být přínos k rozvoji lokální infrastruktury.
+          </p>
+        </div>
+      </section>
+
 
       {/* Call-to-action Section */}
       <section
